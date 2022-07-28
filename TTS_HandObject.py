1a7a5241d2ad6d8f1eb7b44c3b7bc4cee5fe40e1
NicoGitSoft <nicolas.ibanez.r@usach.cl>
2022-07-27 23:33:03 -0400

    .

--- TTS_HandObject.py
+++ TTS_HandObject.py
@@ -56,10 +56,16 @@ def detect_fingers(results_hands):
         y4 = int(hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y * height)
     return [(x5,y5),(x1,y1),(x2,y2),(x3,y3),(x4,y4)]
 
+# Create a function for drawing fingertips
+def draw_fingerstips(finges_position, frame):
+    for finger_position in finges_position:
+        cv2.circle(frame, finger_position, 5, (0, 0, 255), -1)
+    return frame
+
 # capture video
 cap1 = cv2.VideoCapture(0)
-success, image = cap1.read()
-height, width, _ = image.shape
+success, frame = cap1.read()
+height, width, _ = frame.shape
 
 # define distance minimum between hand and face
 delta = .05
@@ -72,19 +78,19 @@ PrevFingerDetect  = False
 # create loop to capture video
 while cap1.isOpened():
     # capture frame from video from cap1
-    success, image = cap1.read()
+    success, frame = cap1.read()
     if not success:
         print("Ignoring empty camera frame.")
         continue
 
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    frame.flags.writeable = False
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
   
-    results_hands = hands.process(image)
-    results_face = face_detection.process(image)
+    results_hands = hands.process(frame)
+    results_face = face_detection.process(frame)
 
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    frame.flags.writeable = True
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
     if results_hands.multi_hand_landmarks is not None:
         
@@ -94,18 +100,19 @@ while cap1.isOpened():
 
         # detect fingers position from hands object results_hands
         finges_position = detect_fingers(results_hands)
+        draw_fingerstips(finges_position, frame)
 
-        cv2.circle(image, finges_position[0], 3,(255,0,0), 3)
-        cv2.circle(image, finges_position[1], 3,(255,0,0), 3)
-        cv2.circle(image, finges_position[2], 3,(255,0,0), 3)
-        cv2.circle(image, finges_position[3], 3,(255,0,0), 3)
-        cv2.circle(image, finges_position[4], 3,(255,0,0), 3)
+        #cv2.circle(frame, finges_position[0], 3,(255,0,0), 3)
+        #cv2.circle(frame, finges_position[1], 3,(255,0,0), 3)
+        #cv2.circle(frame, finges_position[2], 3,(255,0,0), 3)
+        #cv2.circle(frame, finges_position[3], 3,(255,0,0), 3)
+        #cv2.circle(frame, finges_position[4], 3,(255,0,0), 3)
 
-        cv2.putText(image, "5", finges_position[0], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
-        cv2.putText(image, "1", finges_position[1], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
-        cv2.putText(image, "2", finges_position[2], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
-        cv2.putText(image, "3", finges_position[3], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
-        cv2.putText(image, "4", finges_position[4], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+        cv2.putText(frame, "5", finges_position[0], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+        cv2.putText(frame, "1", finges_position[1], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+        cv2.putText(frame, "2", finges_position[2], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+        cv2.putText(frame, "3", finges_position[3], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+        cv2.putText(frame, "4", finges_position[4], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
         
 
         if results_face.detections:
@@ -113,30 +120,30 @@ while cap1.isOpened():
                 # Ojo derecho
                 x_RE = int(detection.location_data.relative_keypoints[0].x * width)
                 y_RE = int(detection.location_data.relative_keypoints[0].y * height)
-                cv2.circle(image, (x_RE, y_RE), 3, (0, 0, 255), 5)
+                cv2.circle(frame, (x_RE, y_RE), 3, (0, 0, 255), 5)
                 # Ojo izquierdo
                 x_LE = int(detection.location_data.relative_keypoints[1].x * width)
                 y_LE = int(detection.location_data.relative_keypoints[1].y * height)
-                cv2.circle(image, (x_LE, y_LE), 3, (255, 0, 255), 5)
+                cv2.circle(frame, (x_LE, y_LE), 3, (255, 0, 255), 5)
                 # Punta de la nariz
                 x_NT = int(detection.location_data.relative_keypoints[2].x * width)
                 y_NT = int(detection.location_data.relative_keypoints[2].y * height)
-                cv2.circle(image, (x_NT, y_NT), 3, (255, 0, 0), 5)
+                cv2.circle(frame, (x_NT, y_NT), 3, (255, 0, 0), 5)
 
             pos = [(x_RE,y_RE),(x_LE,y_LE),(x_NT,y_NT)]
             stringObjets = ["Left-Eye", "Rigth-Eye", "Nose"]
             sumCateros = [abs(x-finges_position[1][0]) + abs(y-finges_position[1][1]) for x,y in pos]
             nearObjectIndex = sumCateros.index(min(sumCateros))
-            cv2.line(image, finges_position[1], pos[nearObjectIndex], (255,0,255), 4)
-            cv2.putText(image, stringObjets[nearObjectIndex], pos[nearObjectIndex], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
+            cv2.line(frame, finges_position[1], pos[nearObjectIndex], (255,0,255), 4)
+            cv2.putText(frame, stringObjets[nearObjectIndex], pos[nearObjectIndex], cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2)
     
     else:
         if PrevFingerDetect:
             speak("Lost Hand") 
             PrevFingerDetect=False
 
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('manitos y cara', image) #cv2.flip(image, 1))
+    # Flip the frame horizontally for a selfie-view display.
+    cv2.imshow('manitos y cara', frame) #cv2.flip(frame, 1))
 
     if cv2.waitKey(5) & 0xFF == 27:
         break
