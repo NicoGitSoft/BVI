7ded1f7558a636b9e6396ec0f5ab612f93e8da62
NicoGitSoft <nicolas.ibanez.r@usach.cl>
2023-01-25 12:20:04 -0300

    .

--- Scripts/Experiments/Bluetooth.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from bluetooth import *
-
-print("looking for bluetooth devices...")
-devices = discover_devices(lookup_names=True)
-
-for addr, name in devices:
-    print("  %s - %s" % (addr, name))
-print("done")
