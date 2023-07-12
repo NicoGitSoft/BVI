8d0418bf3d57b7c4221249501fac2dffd27f4bc1
nicolas.ibanez.r@usach.cl <nicolas.ibanez.r@usach.cl>
2023-07-12 00:12:08 -0400

    upload changes

--- Scripts/Data Science/Communication interface activity logs/Obstacle proximity alerts/Obstacle proximity alerts.py
+++ Scripts/Data Science/Communication interface activity logs/Obstacle proximity alerts/Obstacle proximity alerts.py
@@ -42,42 +42,24 @@ ax = fig.add_axes([deltaX, deltaY + offsetY, 1 - 2 * deltaX, 1 - 2 * deltaY])
 plt.grid(color='gray', linestyle=':', alpha=.2) # Agregar grid minor con lineas, punteadas y color gris claro
 # Graficar mensajes de la interfaz háptica como lineas verticales
 for i in range(N):
-    #if haptic_messages[i] == "l":
-    #    # graficar una linea vertical azul delgada con un text" en la parte superior
-    #    plt.axvline(x=times[i], color='b', linewidth=0.5)
-    #if haptic_messages[i] == "r":
-    #    # graficar una linea vertical roja delgada con un texto "r" en la parte superior
-    #    plt.axvline(x=times[i], color='r', linewidth=0.5)
-    #if haptic_messages[i] == "d":
-    #    # graficar una linea vertical verde delgada con un texto "d" en la parte superior
-    #    plt.axvline(x=times[i], color='g', linewidth=0.5)
-    #if haptic_messages[i] == "u":
-    #    # graficar una linea vertical amarilla delgada con un texto "u" en la parte superior
-    #    plt.axvline(x=times[i], color='y', linewidth=0.5)
-    #if haptic_messages[i] == "c":
-    #    # graficar una linea vertical negra delgada con un texto "c" en la parte superior
-    #    plt.axvline(x=times[i], color='k', linewidth=2)
-    #    plt.text(times[i], 1.5, r"$\twonotes$", color='k', fontsize=16, label=r'Informar detección $\twonotes$')
     if buzzer_messages[i] == 1:
         # graficar una linea vertical negra delgada con un texto "b" en la parte superior
         plt.axvline(x=times[i], color='k', linewidth=1.5)
-        plt.text(times[i], 1.5, r"$\eighthnote$", color='k', fontsize=16, label=r'$\eighthnote$ Report detection')
-#plt.plot(times, h, label=r'Distancia horizontal')
-#plt.plot(times, v, label=r'Distancia vertical')
+        plt.text(times[i], 1.5, r"$\eighthnote$", color='k', fontsize=16, label=r'Detection of an obstacle $\eighthnote$')
 
-plt.plot(times, z, color='r', label=r'Average distance to the OAK-D sensor in the ROI')
+plt.plot(times, z, color='r', label=r'Measured distance to the test obstacle')
 plt.axhline(y=1, color='k', linestyle="--" , linewidth=1, alpha=0.5)
-plt.axhline(y=2, color='k', linestyle="--" , linewidth=1, alpha=0.5, label=r'Obstacle proximity threshold')
+plt.axhline(y=2, color='k', linestyle="--" , linewidth=1, alpha=0.5, label=r'Upper and lower threshold for alerts')
 
 #plt.plot(times, d, label=r'detected')
 #plt.axhline(y=DELTA, color='k', linestyle="--" , linewidth=1, alpha=0.5, label=r'Umbral de detección')
-plt.plot([], [], ' ',color='k', label=r'$\eighthnote$ ROI obstacle alert')
-plt.title(r"Obstacle proximity alerts")
+plt.plot([], [], ' ',color='k', label=r'Alert of an obstacle detected $\eighthnote$')
+#plt.title(r"Obstacle proximity alerts")
 plt.xlabel(r'Time [s]')
 plt.ylabel(r'Distance [m]')
 plt.legend(loc='upper right')
 plt.xlim(times[0], times[-1])
-plt.savefig('Obstacle proximity alerts.svg', format='svg', dpi=1200)
-plt.savefig('Obstacle proximity alerts.png', format='png', dpi=1200)
-plt.savefig('Obstacle proximity alerts.pdf', format='pdf', dpi=1200)
+plt.savefig('Test obstacle proximity alerts.svg', format='svg', dpi=1200, bbox_inches='tight')
+plt.savefig('Test obstacle proximity alerts.png', format='png', dpi=1200, bbox_inches='tight')
+plt.savefig('Test obstacle proximity alerts.pdf', format='pdf', dpi=1200, bbox_inches='tight')
 plt.show()
